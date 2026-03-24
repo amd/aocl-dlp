f9775fcc874ebc776dd21dbaeab9a25f9bc524b7
V, Varsha <Varsha.V@amd.com>
2026-03-24 16:42:35 +0530

    BugFix: BF16 AVX2 fallback GEMV m=1 path for reordered B input. (#484)
    - For BF16 GEMVM1 fallback path when B matrix is reordered, there wasn't a panel
     adjustment happening after the kernel execution.
     - When the input size exceeds the panel boundary this would cause wrong panel
     access leading to incorrect results. Hence, added the same.
    
    [ AMD-Internal : CPUPL-8201 ]

--- classic/frame/bf16bf16f32/lpgemm_bf16.c
+++ classic/frame/bf16bf16f32/lpgemm_bf16.c
@@ -947,6 +947,10 @@ LPGEMV_F32_FALLBACK(bfloat16, bfloat16, float, bf16bf16f32of32)
                        beta, f32_NR, KC, n_sub_updated, jc_cur_loop_rem,
                        post_op_list, &post_ops_attr);
             }
+
+            if (mtag_b == REORDERED) {
+                adjust_B_panel_reordered_jc(&jc, jc_cur_loop);
+            }
         }
 
         if (b_unreorder != NULL) {
